02186197897f762a061801e7a88868515763f4c7
Jason <jason.leitao@flipsidegroup.com>
2023-01-31 15:27:15 +0000

    firebase auth setup

---
+++ nomadlodge/lib/config.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: do_not_use_environment, constant_identifier_names
+
+const TWITTER_API_KEY = String.fromEnvironment('TWITTER_API_KEY');
+const TWITTER_API_SECRET_KEY = String.fromEnvironment('TWITTER_API_SECRET_KEY');
+const TWITTER_REDIRECT_URI = 'ffire://';
+
+const FACEBOOK_CLIENT_ID = '128693022464535';
