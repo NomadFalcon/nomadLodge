ef4e4c01cc3861b37680c2536c1f7a2e76d145c2
jayLeitaoResideo <jason.leitao@resideo.com>
2024-03-08 15:09:30 -0500

    long time fixes

--- nomadlodge/macos/Flutter/GeneratedPluginRegistrant.swift
+++ nomadlodge/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,20 +7,26 @@ import Foundation
 
 import cloud_firestore
 import desktop_webview_auth
+import facebook_auth_desktop
 import firebase_analytics
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
+import flutter_secure_storage_macos
 import package_info
+import path_provider_foundation
 import twitter_login
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   TwitterLoginPlugin.register(with: registry.registrar(forPlugin: "TwitterLoginPlugin"))
 }
