e53a1e87b854b919dc12eec1a7efb96adbebcaa5
jayLeitaoResideo <jason.leitao@resideo.com>
2024-03-08 16:25:05 -0500

    added server

--- nomadlodge/macos/Flutter/GeneratedPluginRegistrant.swift
+++ nomadlodge/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import connectivity_plus
 import desktop_webview_auth
 import facebook_auth_desktop
 import firebase_analytics
@@ -19,6 +20,7 @@ import twitter_login
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
---
+++ nomadlodge/nomadlodge_backend/nomadlodge_backend_flutter/android/app/src/main/kotlin/com/example/nomadlodge_backend_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nomadlodge_backend_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
