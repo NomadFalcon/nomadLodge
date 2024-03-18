af8b7ecdd23eddd6f8111888dac485ddc910aed9
jayLeitaoResideo <jason.leitao@resideo.com>
2024-03-18 13:30:01 -0400

    added location and task endpoints

---
+++ nomadlodge/nomadlodge_backend/nomadlodge_backend_server/lib/src/endpoints/location_endpoint.dart
@@ -0,0 +1,37 @@
+import 'package:nomadlodge_backend_server/src/generated/protocol.dart';
+import 'package:serverpod/serverpod.dart';
+
+// This is an example endpoint of your server. It's best practice to use the
+// `Endpoint` ending of the class name, but it will be removed when accessing
+// the endpoint from the client. I.e., this endpoint can be accessed through
+// `client.example` on the client side.
+
+// After adding or modifying an endpoint, you will need to run
+// `serverpod generate` to update the server and client code.
+class LocationEndpoint extends Endpoint {
+  // You create methods in your endpoint which are accessible from the client by
+  // creating a public method with `Session` as its first parameter.
+  // `bool`, `int`, `double`, `String`, `UuidValue`, `Duration`, `DateTime`, `ByteData`,
+  // and other serializable classes, exceptions and enums from your from your `protocol` directory.
+  // The methods should return a typed future; the same types as for the parameters are
+  // supported. The `session` object provides access to the database, logging,
+  // passwords, and information about the request being made to the server.
+  Future<List<Location>> getAll(Session session) async {
+    final locations = <Location>[];
+    return locations;
+  }
+
+  Future<Location?> getById(Session session, int locationId) async {
+    return null;
+  }
+
+  Future<List<LocationTeam>> getTeams(Session session, int locationId) async {
+    final teams = <LocationTeam>[];
+    return teams;
+  }
+
+  Future<List<Booking>> getBookings(Session session, int locationId) async {
+    final bookings = <Booking>[];
+    return bookings;
+  }
+}
