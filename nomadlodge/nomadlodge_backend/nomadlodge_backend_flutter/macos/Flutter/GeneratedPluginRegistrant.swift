3593fb3ff05a94e9785de669e4abb49bff5209c0
jayLeitaoResideo <jason.leitao@resideo.com>
2024-10-22 09:41:11 +0100

    added external UI elements and new packages

--- nomadlodge/nomadlodge_backend/nomadlodge_backend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ nomadlodge/nomadlodge_backend/nomadlodge_backend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,10 @@ import file_selector_macos
 import firebase_core
 import firebase_messaging
 import path_provider_foundation
+import quill_native_bridge_macos
 import shared_preferences_foundation
 import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
@@ -21,6 +23,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
